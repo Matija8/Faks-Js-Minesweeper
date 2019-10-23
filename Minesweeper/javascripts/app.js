6bb311495eaa7aa9ee4159997d9bcaa104723f0a
matija8 <matijanme@gmail.com>
2019-10-23 22:13:27 +0200

    Added basic clicking ability to table cells.

--- Minesweeper/javascripts/app.js
+++ Minesweeper/javascripts/app.js
@@ -7,15 +7,27 @@ var num_of_cols = 7;
 //get the game area (table).
 var table = document.getElementById("play_area");
 
+//call func.
+initializePlayArea(table, num_of_cols, num_of_rows);
+
 
 //Insert rows and cells into the table.
-for(var i = 0; i<num_of_rows; i++){
-    var table_row = document.createElement("tr");
+function initializePlayArea(table, num_of_cols = 10, num_of_rows = 20){
+    for(var i = 0; i<num_of_rows; i++){
+        var table_row = document.createElement("tr");
 
-    for(var j = 0; j<num_of_cols; j++){
-        var table_cell = document.createElement("td");
-        table_cell.innerHTML = i + "," + j;
-        table_row.appendChild(table_cell);
+        for(var j = 0; j<num_of_cols; j++){
+            var table_cell = document.createElement("td");
+            table_cell.innerHTML = i + "," + j;
+            table_cell.setAttribute("id" , i + "," + j);
+            table_row.appendChild(table_cell);
+        }
+        table.appendChild(table_row);
     }
-    table.appendChild(table_row);
+}
+
+document.getElementById("0,0").addEventListener("click", testFunc);
+
+function testFunc(){
+    alert("TEST!");
 }
