44a215c5e24d27c91633709afb1f360d9af73590
matija8 <matijanme@gmail.com>
2019-10-23 22:00:51 +0200

    Added play area initialization (number of rows and columns).

--- Minesweeper/javascripts/app.js
+++ Minesweeper/javascripts/app.js
@@ -1,1 +1,21 @@
-//alert("Radi app.js!")
+"use strict"
+
+//set as desired.
+var num_of_rows = 5;
+var num_of_cols = 7;
+
+//get the game area (table).
+var table = document.getElementById("play_area");
+
+
+//Insert rows and cells into the table.
+for(var i = 0; i<num_of_rows; i++){
+    var table_row = document.createElement("tr");
+
+    for(var j = 0; j<num_of_cols; j++){
+        var table_cell = document.createElement("td");
+        table_cell.innerHTML = i + "," + j;
+        table_row.appendChild(table_cell);
+    }
+    table.appendChild(table_row);
+}
