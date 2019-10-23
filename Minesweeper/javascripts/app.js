c78743ab646017e7419c88bf5dbabfec7bc3098e
matija8 <matijanme@gmail.com>
2019-10-23 22:48:24 +0200

    Added event listeners on all table cells.

--- Minesweeper/javascripts/app.js
+++ Minesweeper/javascripts/app.js
@@ -1,33 +1,40 @@
-"use strict"
+'use strict'
 
 //set as desired.
 var num_of_rows = 5;
 var num_of_cols = 7;
 
 //get the game area (table).
-var table = document.getElementById("play_area");
-
-//call func.
+var table = document.getElementById('play_area');
 initializePlayArea(table, num_of_cols, num_of_rows);
 
 
-//Insert rows and cells into the table.
 function initializePlayArea(table, num_of_cols = 10, num_of_rows = 20){
+
+    //adding rows and columns.
     for(var i = 0; i<num_of_rows; i++){
-        var table_row = document.createElement("tr");
+        var table_row = document.createElement('tr');
 
         for(var j = 0; j<num_of_cols; j++){
-            var table_cell = document.createElement("td");
-            table_cell.innerHTML = i + "," + j;
-            table_cell.setAttribute("id" , i + "," + j);
+            var table_cell = document.createElement('td');
+            table_cell.innerHTML = i + ',' + j;
+            table_cell.classList.add('table-cell');
+            table_cell.setAttribute('id' , i + ',' + j);
             table_row.appendChild(table_cell);
         }
         table.appendChild(table_row);
     }
 }
 
-document.getElementById("0,0").addEventListener("click", testFunc);
 
-function testFunc(){
-    alert("TEST!");
+function testFunc(id){
+    console.log('ID is: ' + id);
 }
+
+
+//arrow functions in es6! =>
+document.querySelectorAll('.table-cell').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log('ID is: ' + item.id);
+    })
+  })
