281c8b4146bd3ff7d9cdaf9b25f7a84aa1a27fa8
matija8 <matijanme@gmail.com>
2019-11-03 22:21:52 +0100

    Added event listeners.

--- Minesweeper/javascripts/app.js
+++ Minesweeper/javascripts/app.js
@@ -1,77 +1,93 @@
 'use strict';
 /* jshint browser: true */
+/*jshint esversion: 6 */
 
 //Initial play area dimensions. Set as desired.
 var numOfRows = 7,
     numOfCols = 9,
     numOfMines = 5;
 
-new Game(numOfRows, numOfCols, numOfMines);
-
-document.getElementById('new-game').addEventListener('click', function(event){ location.reload(); });
-
 
 //Object constructors:
+class Game {
+    constructor(numOfRows, numOfCols, numOfMines) {
+        this.numOfLeftClicked = 0;
+        this.numOfRightClicked = 0;
+        this.seconds = 0;
+        this.firstClick = true;
+        this.cellMatrix = [];
+        this.initPlayArea();
+        this.setAdjacentCells(this.cellMatrix);
+    }//constructor
 
-function Game(numOfRows, numOfCols, numOfMines){
-    this.numOfLeftClicked = 0;
-    this.numOfRightClicked = 0;
-    this.seconds = 0;
-    this.firstClick = true;
-    this.cellMatrix = [];
-
-    initPlayArea(this);
-    setAdjacentCells(this.cellMatrix);
-
-    //Making the play-area.
-    function initPlayArea(game){
+    initPlayArea() {
         var tableDOM = document.createElement('div');
-            tableDOM.setAttribute('id', 'play-area');
-        for(var i = 0; i<numOfRows; i++){
-            var rowDOM = document.createElement('div'),
-                row = [];
-                rowDOM.classList.add('row');
-            for(var j = 0; j<numOfCols; j++){
+        tableDOM.setAttribute('id', 'play-area');
+        for (var i = 0; i < numOfRows; i++) {
+            var rowDOM = document.createElement('div'), row = [];
+            rowDOM.classList.add('row');
+            for (var j = 0; j < numOfCols; j++) {
                 var cellDOM = document.createElement('div');
-                    cellDOM.classList.add('cell');
-                    cellDOM.innerText = i + ',' + j;
+                cellDOM.classList.add('cell');
+                cellDOM.innerText = i + ',' + j;
                 rowDOM.appendChild(cellDOM);
                 row.push(new Cell(cellDOM));
             }
-            game.cellMatrix.push(row);
+            this.cellMatrix.push(row);
             tableDOM.appendChild(rowDOM);
         }
         document.getElementById('play-area-container').appendChild(tableDOM);
-    }
+    }//initPlayArea
 
-
-    //Setting adjacent cells.
-    function setAdjacentCells(matrix){
-        for(var i = 0; i<numOfRows; i++){
-            for(var j = 0; j<numOfCols; j++){
+    setAdjacentCells(matrix) {
+        for (var i = 0; i < numOfRows; i++) {
+            for (var j = 0; j < numOfCols; j++) {
                 matrix[i][j].adjacent.push('test');
             }
         }
-    }
+    }//setAdjacentCells
 
+}//class Game
+
+
+class Cell {
+    constructor(item) {
+        this.item = item; //DOM object reference
+        this.mineState = 'no-mine'; // {'no-mine', 'MINE!'} 
+        this.clickState = 'not-clicked'; // {'not-clicked', 'left-clicked', 'right-clicked'}
+        this.adjacent = [];
+        this.item.addEventListener('mousedown', function (event) { mouseDown(item, event); });
+        this.item.addEventListener('mouseup', function (event) { mouseUp(item, event); });
+        function mouseDown(item, event) {
+            switch(event.button) {
+                case 0: //leftClick
+                    console.log('left');
+                    break;
+                case 1: //leftClick
+                    console.log('mid');
+                    break;
+                case 2: //leftClick
+                    console.log('right');
+                    break;
+            }
+        }
+        function mouseUp(item, event) {
+            switch(event.button) {
+                case 0: //leftClick
+                    console.log('left');
+                    break;
+                case 1: //leftClick
+                    console.log('mid');
+                    break;
+                case 2: //leftClick
+                    console.log('right');
+                    break;
+            }
+        }
+    }
 }
 
 
-function Cell(item){
-    this.item = item;                   //DOM object reference
-    this.mineState = 'no-mine';         // {'no-mine', 'MINE!'} 
-    this.clickState = 'not-clicked';    // {'not-clicked', 'left-clicked', 'right-clicked'}
-    this.adjacent = [];
+new Game(numOfRows, numOfCols, numOfMines);
 
-    this.item.addEventListener('mousedown', function(event){ mouseDown(item, event); });
-    this.item.addEventListener('mouseup', function(event){ mouseUp(item, event); });
-
-
-    function mouseDown(item, event){
-        console.log(item, event);
-    }
-
-    function mouseUp(item, event){
-        console.log(item, event);
-    }
-}
+document.getElementById('new-game').addEventListener('click', function(event){ location.reload(); });
