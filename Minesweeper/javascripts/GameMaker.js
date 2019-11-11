37659ebdb5f431c0adfa0dea51b0fe9bb0602200
matija8 <matijanme@gmail.com>
2019-11-11 19:55:46 +0100

    Trying to make the game mode changing work.

--- Minesweeper/javascripts/GameMaker.js
+++ Minesweeper/javascripts/GameMaker.js
@@ -12,23 +12,61 @@
 class GameMaker {
     constructor(newGameButton, gameParentNode){
         //Defaults:
-        this.NumOfRows  = 8;
-        this.NumOfCols  = 8;
-        this.NumOfMines = 10;
+        this.NumOfRows  = 16;
+        this.NumOfCols  = 16;
+        this.NumOfMines = 40;
         this.CellSize   = '45px';
         this.FontSize   = '22px';
-        //TODO get setup from radio buttons.
-        document.getElementById('default').checked = true;
-        //this.checked = document.querySelector('input[name="mode"]:checked').value;
+        this.game       = null;
+        this.radios = document.querySelectorAll('.drop-radio');
+        const   defaultRadio = document.getElementById('default');
+                this.checked = defaultRadio.value;
+                this.prevChecked = this.checked;
+                defaultRadio.checked = true;
 
-        this.game = new Game(this.NumOfRows, this.NumOfCols, this.NumOfMines, new Style(this.CellSize, this.FontSize), gameParentNode);
         this.newGameButton = newGameButton;
-
         this.newGameButton.addEventListener('click', () => { this.newGame(); });
         document.addEventListener('keydown', e => { if (e.which == 113) this.newGame(); } );
+        this.radios.forEach(radio => {
+            radio.addEventListener('click', () => {this.checked = document.querySelector('.drop-radio:checked').value; //TODO: better solution.
+                console.log(this.checked);
+            });
+        });
+        this.gameMake();
     }
 
     newGame(){
-        this.game.reInit();
+        if(this.checked !== this.prevChecked){
+            this.prevChecked = this.checked;
+            this.gameMake(); //make a new game if the dimensions have changed.
+        }
+        else{
+            this.game.reInit();
+        }
+    }
+
+    gameMake(){
+        this.setDimensions(this.checked);
+        this.game = new Game(this.NumOfRows, this.NumOfCols, this.NumOfMines, new Style(this.CellSize, this.FontSize), gameParentNode);
+    }
+
+    setDimensions(mode){
+        switch(mode){
+            case 1:
+                this.NumOfRows  = 8;
+                this.NumOfCols  = 8;
+                this.NumOfMines = 10;
+                break;
+            case 2:
+                this.NumOfRows  = 16;
+                this.NumOfCols  = 16;
+                this.NumOfMines = 40;
+                break;
+            case 3:
+                this.NumOfRows  = 30;
+                this.NumOfCols  = 16;
+                this.NumOfMines = 99;
+                break;
+        }
     }
 }
